cf512d04598b828d051e0651b00ba31ca332da3e
Leonard Struck <me@leonardstruck.com>
2023-03-04 16:37:22 +0100

    add parameter to store

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,21 +2,33 @@ import clsx from "clsx";
 import { useEffect, useState } from "react";
 import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid"
 
+import useStore from "../store";
+
+import { v4 as uuid } from "uuid";
+
 const Table = () => {
+    const { parameters } = useStore();
     return (
         <div className="text-white">
             <div className="grid grid-cols-6 gap-2">
-                <Box>Parameters</Box>
-                <Box colSpan={4}>Characteristics</Box>
+                <Box primary>Parameters</Box>
+                <Box primary colSpan={4}>Characteristics</Box>
+                {parameters.map((parameter, index) => (
+                    <div key={parameter.id} className="grid grid-cols-6 gap-2 col-span-6">
+                        <Box>{parameter.name}</Box>
+                        <Box colSpan={4}>
+                        </Box>
+                    </div>
+                ))}
                 <AddParameter />
             </div>
         </div>
     )
 }
 
-const Box = ({ children, colSpan }: { children: React.ReactNode, colSpan?: 2 | 4 }) => {
+const Box = ({ children, colSpan, primary }: { children: React.ReactNode, colSpan?: 2 | 4, primary?: boolean }) => {
     return (
-        <div className={clsx(colSpan == 4 && "col-span-4", colSpan == 2 || !colSpan && "col-span-2", "h-12 border rounded flex justify-center items-center shadow-md shadow-gray-700 font-bold text-xl font-mono cursor-default")}>
+        <div className={clsx(colSpan == 4 && "col-span-4", colSpan == 2 || !colSpan && "col-span-2", !primary ? "bg-gray-800 text-sm" : "text-xl", "h-12 border rounded flex justify-center items-center shadow-md shadow-gray-700 font-bold font-mono cursor-default")}>
             {children}
         </div>
     )
@@ -26,10 +38,20 @@ const AddParameter = () => {
     const [isActive, setIsActive] = useState(false);
     const [name, setName] = useState("");
 
+    const { addParameter } = useStore();
+
+    const handleAdd = () => {
+        addParameter({
+            name,
+            id: uuid(),
+        });
+        setName("");
+    }
+
     useEffect(() => {
         const handleKeyDown = (e: KeyboardEvent) => {
             if (e.key === "Enter") {
-                setName("");
+                handleAdd();
             }
             if (e.key === "Escape") {
                 setIsActive(false);
@@ -42,7 +64,7 @@ const AddParameter = () => {
         return () => {
             window.removeEventListener("keydown", handleKeyDown);
         }
-    }, []);
+    }, [handleAdd, setName, setIsActive]);
 
     if (!isActive) return (
         <div
@@ -55,7 +77,7 @@ const AddParameter = () => {
     return (
         <div className="col-span-6 h-12 border rounded flex justify-center items-center shadow-md shadow-gray-700 font-bold text-xl font-mono pr-2">
             <input type="text" className="bg-transparent text-white w-full text-2xl border-0 focus:border-transparent focus:ring-0" value={name} onChange={(e) => setName(e.currentTarget.value)} />
-            <div className="w-8 hover:text-green-600 cursor-pointer">
+            <div className="w-8 hover:text-green-600 cursor-pointer" onClick={() => handleAdd()}>
                 <CheckIcon />
             </div>
             <div className="w-8 hover:text-red-600 cursor-pointer" onClick={() => {
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,96 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
+
+export type Parameter = {
+    id: string;
+    name: string;
+    selectedCharacteristicId?: string;
+}
+
+export type Characteristic = {
+    id: string;
+    parameterId: string;
+    name: string;
+};
+
 export interface Store {
     apiKey?: string;
     setApiKey: (apiKey: string) => void;
+
+    parameters: Parameter[];
+    setParameters: (parameters: Parameter[]) => void;
+
+    characteristics: Characteristic[];
+    setCharacteristics: (characteristics: Characteristic[]) => void;
+
+    addParameter: (parameter: Parameter) => void;
+    addCharacteristic: (characteristic: Characteristic) => void;
+
+    removeParameter: (parameterId: string) => void;
+    removeCharacteristic: (characteristicId: string) => void;
+
+    getCharacteristigByParameterId: (parameterId: string) => Characteristic[];
 }
 
 const useStore = create(
     persist<Store>(
         (set, get) => ({
             apiKey: undefined,
-            setApiKey: (apiKey: string) => set({ apiKey })
+            setApiKey: (apiKey: string) => set({ apiKey }),
+
+            parameters: [],
+            setParameters: (parameters: Parameter[]) => set({ parameters }),
+
+            characteristics: [],
+            setCharacteristics: (characteristics: Characteristic[]) => set({ characteristics }),
+
+            addParameter: (parameter: Parameter) => {
+                if (parameter.name === "" || parameter.name === undefined) return;
+                const parameters = get().parameters;
+                parameters.push(parameter);
+                set({ parameters });
+            },
+
+            addCharacteristic: (characteristic: Characteristic) => {
+                if (characteristic.name === "" || characteristic.name === undefined) return;
+                const characteristics = get().characteristics;
+                characteristics.push(characteristic);
+                set({ characteristics });
+            },
+
+            removeParameter: (parameterId: string) => {
+                const parameters = get().parameters;
+                const index = parameters.findIndex((parameter) => parameter.id === parameterId);
+                if (index !== -1) {
+                    parameters.splice(index, 1);
+
+                    const characteristics = get().characteristics;
+                    const characteristicsToRemove = characteristics.filter((characteristic) => characteristic.parameterId === parameterId);
+                    characteristicsToRemove.forEach((characteristic) => {
+                        const index = characteristics.findIndex((characteristic) => characteristic.id === characteristic.id);
+                        if (index !== -1) {
+                            characteristics.splice(index, 1);
+                        }
+                    });
+
+                    set({ parameters, characteristics });
+                }
+            },
+
+            removeCharacteristic: (characteristicId: string) => {
+                const characteristics = get().characteristics;
+                const index = characteristics.findIndex((characteristic) => characteristic.id === characteristicId);
+                if (index !== -1) {
+                    characteristics.splice(index, 1);
+                    set({ characteristics });
+                }
+            },
+
+            getCharacteristigByParameterId: (parameterId: string) => {
+                const characteristics = get().characteristics;
+                return characteristics.filter((characteristic) => characteristic.parameterId === parameterId);
+            }
         }),
         {
             name: "store",
