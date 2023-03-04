1839f0b61dda57ab64527d9f00f62e59da1c4b01
Leonard Struck <me@leonardstruck.com>
2023-03-04 16:45:02 +0100

    interface improvements

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
-import { useEffect, useState } from "react";
-import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid"
+import { useEffect, useRef, useState } from "react";
+import { CheckIcon, XMarkIcon, PlusCircleIcon } from "@heroicons/react/24/solid"
 
 import useStore from "../store";
 
@@ -28,7 +28,7 @@ const Table = () => {
 
 const Box = ({ children, colSpan, primary }: { children: React.ReactNode, colSpan?: 2 | 4, primary?: boolean }) => {
     return (
-        <div className={clsx(colSpan == 4 && "col-span-4", colSpan == 2 || !colSpan && "col-span-2", !primary ? "bg-gray-800 text-sm" : "text-xl", "h-12 border rounded flex justify-center items-center shadow-md shadow-gray-700 font-bold font-mono cursor-default")}>
+        <div className={clsx(colSpan == 4 && "col-span-4", colSpan == 2 || !colSpan && "col-span-2", !primary ? "bg-gray-800 text-sm" : "text-xl", "min-h-12 border rounded flex justify-center items-center shadow-md shadow-gray-700 font-bold font-mono cursor-default p-4")}>
             {children}
         </div>
     )
@@ -37,6 +37,8 @@ const Box = ({ children, colSpan, primary }: { children: React.ReactNode, colSpa
 const AddParameter = () => {
     const [isActive, setIsActive] = useState(false);
     const [name, setName] = useState("");
+
+    const parameterInput = useRef<HTMLInputElement>(null);
 
     const { addParameter } = useStore();
 
@@ -66,17 +68,23 @@ const AddParameter = () => {
         }
     }, [handleAdd, setName, setIsActive]);
 
+    useEffect(() => {
+        if (isActive) {
+            parameterInput.current?.focus();
+        }
+    }, [isActive]);
+
     if (!isActive) return (
         <div
             className="col-span-2 h-12 border rounded flex justify-center items-center shadow-md shadow-gray-700 font-bold text-sm font-mono bg-green-800 cursor-pointer"
             onClick={() => setIsActive(true)}>
-            <span>+ add parameter</span>
+            <span className="flex gap-2 items-center"><div className="w-6 h-6"><PlusCircleIcon /></div>Add Parameter</span>
         </div>
     );
 
     return (
-        <div className="col-span-6 h-12 border rounded flex justify-center items-center shadow-md shadow-gray-700 font-bold text-xl font-mono pr-2">
-            <input type="text" className="bg-transparent text-white w-full text-2xl border-0 focus:border-transparent focus:ring-0" value={name} onChange={(e) => setName(e.currentTarget.value)} />
+        <div className="col-span-2 h-12 border rounded flex justify-center items-center shadow-md shadow-gray-700 font-bold text-xl font-mono pr-2">
+            <input ref={parameterInput} type="text" className="bg-transparent text-white w-full text-2xl border-0 focus:border-transparent focus:ring-0" value={name} onChange={(e) => setName(e.currentTarget.value)} />
             <div className="w-8 hover:text-green-600 cursor-pointer" onClick={() => handleAdd()}>
                 <CheckIcon />
             </div>
